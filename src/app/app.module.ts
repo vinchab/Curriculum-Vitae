e74fe452985f23223e8218dc0523b9b98ecaa604
vinchab <v.chablaix@gmail.com>
2021-02-12 17:40:35 +0100

    add initialize Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// FIREBASE
+import { AngularFireModule } from '@angular/fire'
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +27,9 @@ import { ProfessionalExperienceComponent } from './components/professional-exper
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  
+  firebaseConfig : {
+    apiKey: "AIzaSyDCDMqfUBY7RNoi9a7ig7Vq_P1pt09_10o",
+    authDomain: "my-cv-d02c9.firebaseapp.com",
+    projectId: "my-cv-d02c9",
+    storageBucket: "my-cv-d02c9.appspot.com",
+    messagingSenderId: "363874898860",
+    appId: "1:363874898860:web:fdd38d087af33cbda76009"
+  }
 };
 
 /*
